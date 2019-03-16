06456ad1e51667ec83425ca87352b60007916bc9
aayushdutt <aayushd100@gmail.com>
2019-03-16 20:18:01 +0530

    in the middle of data loading into master database

--- backend/scoreapp/db_utils.py
+++ backend/scoreapp/db_utils.py
@@ -5,10 +5,24 @@ from sqlalchemy import Column, Integer, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import csv
+
+def load_data(file_name):
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        cnt = 0
+        for row in csv_reader:
+            print(row)
+            print(type(row))
+            cnt += 1
+            if cnt > 10: 
+                break
+
 
 # def Load_Data(file_name):
 #     data = genfromtxt(file_name, delimiter=',', skip_header=1, converters={0: lambda s: str(s)})
 #     return data.tolist()
 #
 #
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    load_data('ml/data.csv')
--- backend/scoreapp/routes.py
+++ backend/scoreapp/routes.py
@@ -1,11 +1,13 @@
 import os
+import csv
 import secrets
+import pandas as pd
 # import numpy as np
 # from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, send_file, jsonify
 from scoreapp import app, db, bcrypt
 from scoreapp.forms import LoginForm
-from scoreapp.models import order, shippingData, registrationData, marketing, jobs, teamUser
+from scoreapp.models import order, shippingData, registrationData, marketing, jobs, teamUser, masterData
 from flask_login import login_user, current_user, logout_user, login_required
 from scoreapp.job_helper import *
 
@@ -70,3 +72,35 @@ def run(category=None):
 def logout():
 	logout_user()
 	return redirect(url_for('login'))
+
+
+@app.route("/dataloader")
+def load_data():
+	#file = pd.read_csv('scoreapp/data.csv')
+	#print(file.head())
+	file_name = 'scoreapp/data.csv'
+	with open(file_name) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		flag = 0
+		cnt = 0
+		for row in csv_reader:
+			#print(row)
+			#print(type(row))
+			#age, id, is_auto_billing_on, is_paytm_first, is_postpaid, postpaid_outstanding, orders_placed_in_6months, \
+    #orders_placed_in_6months_via_epay, orders_placed_in_6months_via_cod, orders_placed_in_6months_via_emi, \
+    #orders_delivered_in_6months, total_money_on_order_from_mall_6months, total_money_on_order_on_travel_6months, \
+    #total_money_on_order_on_movie_6months, total_money_spent, total_money_added_on_wallet, CODorNot, EMIorNot
+
+			if flag:
+				md = masterData( id = row[1], age = row[0], is_auto_billing = row[2], is_paytm_first = row[3], 
+				is_postpaid = row[4], postpaid_outstanding = row[5], orders_placed_in_6months = row[6], 
+				orders_placed_in_6months_via_epay = row[7], orders_placed_in_6months_via_cod = row[8],
+				orders_placed_in_6months_via_emi = row[9], orders_delivered_in_6months = row[10], 
+				total_money_on_order_from_mall_6months = row[11], total_money_on_order_on_travel_6months = row[12],
+				total_money_on_order_on_movie_6months = row[13], total_money_spent = row[14], 
+				total_money_added_on_wallet = row[15], CODorNot = row[16], EMIorNot = row[17])
+				db.session.add(md)
+				db.session.commit()
+				cnt += 1
+				print(cnt)				
+			flag = 1
