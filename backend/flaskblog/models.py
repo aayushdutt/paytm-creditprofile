b6eb6f67eddd27e7da326cf4ed4179a197162bcc
aayushdutt <aayushd100@gmail.com>
2019-03-16 10:15:27 +0530

    refactor routes and templates

--- backend/flaskblog/models.py
+++ backend/flaskblog/models.py
@@ -7,17 +7,6 @@ from sqlalchemy import  *
 def load_user(user_id):
     return teamUser.query.get(int(user_id))
 
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-#     password = db.Column(db.String(60), nullable=False)
-#     patient = db.relationship('Patient', backref='author', lazy=True)
-#
-#     def __repr__(self):
-#         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
 
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
